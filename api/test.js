b38e9b6f22544b2798c04b06a46fe32b92a9d44a
Robert Willemelis <robert.dev@gmx.de>
2020-12-12 13:56:59 +0100

    fix: error with api

--- api/test.js
+++ api/test.js
@@ -2,7 +2,6 @@ module.exports = (req, res) => {
   const {
     query: { name },
   } = req;
-  name = name.replace(/<>/ig, '')
 
-  res.send(`Hello ${name}!`);
+  res.send(`Hello ${name.replace(/<>/ig, '')}!`);
 };
